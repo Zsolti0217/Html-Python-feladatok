115a4fca72ffc4e837ec014506b844bc1fdb86d2
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2021-11-29 14:01:50 +0100

    Delete oraifeladat7.py

--- oraifeladat7.py
+++
@@ -1,8 +0,0 @@
-ak = int(input("Hány óra van:  "))
-ker= int(input("hány óra múlva szóljon?:  ") )
-if ker < 24 :
-    eredm1 = ak + ker
-    print(eredm1)
-else:
-    eredm = ak + (round(ker/24))
-    print(eredm)
