9d85d3c95f398e747bd597b156e3fe361c02aeba
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-02-22 09:54:22 +0100

    Testtömeg index
    2022.02.22

---
+++ testomeg szamitas/testomeg szamitas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace testomeg_szamitas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Add meg a testömeged");
+            double testtomeg =Double.Parse( Console.ReadLine());
+           Console.WriteLine("Add meg a testmagasságod");
+            double testmagassag = Double.Parse(Console.ReadLine();
+            double bmi = 0
+            
+            bmi = testtomeg / (testtomeg*testmagassag)
+            
+            if(bmi >= 18,5)
+            {
+                Console.WriteLine("Sovány");
+           
+
+            }
+            
+             else if(bmi > && <= 25)
+            {
+                Console.WriteLine("Normál")
+            }
+
+            else if(bmi > 25 && bmi >=30)
+            {
+                Console.WriteLine("túlsúly")
+            }
+
+            else if(bmi > 30)
+            {
+                Console.WriteLine("Elhízás")
+            }
+
+            Console.ReadLine();
+            
+
+           
+            
+        }
+    }
+}
